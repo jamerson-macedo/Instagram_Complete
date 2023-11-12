a698a15939f85a4cab87bb09e93e4611a2081c62
jamerson Macedo <jamerson_macedo@hotmail.com>
2023-11-11 21:54:53 -0300

    otimizacao de codigo

--- app/src/main/java/com/example/instagram/home/view/HomeActivity.kt
+++ app/src/main/java/com/example/instagram/home/view/HomeActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.view.WindowInsetsController
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.instagram.R
 import com.example.instagram.databinding.ActivityHomeBinding
 import com.example.instagram.profile.view.ProfileFragment
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
@@ -34,6 +36,10 @@ class HomeActivity : AppCompatActivity() {
             WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
             WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
         )
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.photoicon)
+        supportActionBar?.setTitle("")
 
         navigationView = findViewById(R.id.bottom_navigation)
 
@@ -44,29 +50,39 @@ class HomeActivity : AppCompatActivity() {
         profileFragment = ProfileFragment()
 
         binding.bottomNavigation.selectedItemId = R.id.nav_home
+
+
         binding.bottomNavigation.setOnItemSelectedListener { item ->
+            var scroolTollbar = false
             when (item.itemId) {
                 R.id.nav_home -> {
+                    if (currentFragment == homeFragment) return@setOnItemSelectedListener false
                     currentFragment = homeFragment
+                    scroolTollbar = true
                 }
 
                 R.id.nav_search -> {
+                    if (currentFragment == searchFragment) return@setOnItemSelectedListener false
                     currentFragment = searchFragment
                 }
 
                 R.id.nav_publish -> {
+                    if (currentFragment == cameraFragment) return@setOnItemSelectedListener false
                     currentFragment = cameraFragment
                 }
 
                 R.id.nav_reels -> {
+                    if (currentFragment == reelsFragment) return@setOnItemSelectedListener false
                     currentFragment = reelsFragment
                 }
 
                 R.id.nav_profile -> {
+                    if (currentFragment == profileFragment) return@setOnItemSelectedListener false
                     currentFragment = profileFragment
                 }
 
             }
+            setScrollToolbarEnable(scroolTollbar)
             currentFragment?.let {
                 if (supportFragmentManager.findFragmentById(R.id.main_fragment) == null) {
                     supportFragmentManager.beginTransaction().apply {
@@ -81,8 +97,25 @@ class HomeActivity : AppCompatActivity() {
                     }
                 }
             }
-            true
+            return@setOnItemSelectedListener true
         }
+    }
+
+    private fun setScrollToolbarEnable(enabled: Boolean) {
+        // conmfigurando dinamicamente a acao da toolbar
+        var params = binding.toolbar.layoutParams as AppBarLayout.LayoutParams
+        var coordinatorparams = binding.appbar.layoutParams as CoordinatorLayout.LayoutParams
+        if (enabled) {
+            // se for pra ativar ele usa isso
+            params.scrollFlags =
+                AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL or AppBarLayout.LayoutParams.SCROLL_FLAG_ENTER_ALWAYS
+            coordinatorparams.behavior = AppBarLayout.Behavior()
+        } else {
+            // resetando
+            params.scrollFlags = 0
+            coordinatorparams.behavior = null
+        }
+        binding.appbar.layoutParams = coordinatorparams
     }
 }
 
